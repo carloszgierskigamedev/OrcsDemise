9b292d66923a112bf84247466ce9d39d1988a37a
Xanz <carloszgierski@gmail.com>
2019-07-23 23:19:03 -0300

    Acho que agora deu certo

---
+++ Assets/Player/Scripts/PlayerAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 playerPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+        Vector3 playerPosition = transform.position;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+
+            Debug.Log(mousePosition);
+            Debug.DrawLine(playerPosition, mousePosition, Color.red, 2.5f);
+
+        }
+    }
+}
---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private float speed; 
+
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update() 
+    {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2 (horizontal, vertical);
+
+        direction.Normalize();
+
+        rigidbody2D.velocity = direction * speed;
+;
+
+    }
+
+    void FixedUpdate()
+    {
+
+
+
+    }
+}
