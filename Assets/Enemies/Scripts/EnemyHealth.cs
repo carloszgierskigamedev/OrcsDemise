6c3d5851d6fc40d396f84c3307ffcf154d9ab552
Xanz <carloszgierski@gmail.com>
2019-07-25 00:36:32 -0300

    Monstro morre após tomar 100 de dano. Codigo ficou meio merda.

---
+++ Assets/Enemies/Scripts/EnemyHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private float totalHealthPoints;
+    private float currentHealthPoints;
+
+
+    void Awake()
+    {
+        currentHealthPoints = totalHealthPoints;
+    }
+
+    void Update()
+    {
+        if (currentHealthPoints <= 0) 
+        {
+
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D col) 
+    {
+
+        if (col.gameObject.tag.Equals("Projectile"))
+        {
+
+            currentHealthPoints -= 20;
+
+        }
+
+    }
+
+
+}
